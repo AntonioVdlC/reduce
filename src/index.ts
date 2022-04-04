6ad0d784ea82fb1382f779ae2dcd301c613f1276
Antonio Villagra De La Cruz <5470315+AntonioVdlC@users.noreply.github.com>
2022-04-04 18:29:47 +0200

    Implement sum

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-function add(a: number, b: number): number {
-  return a + b;
+import getValueByKey from "get-value-key";
+function sum(acc: number, curr: number): number {
+  return Number(acc) + Number(curr);
 }
+sum.on = (key: string) => {
+  return (
+    acc: Record<string, any> | number,
+    curr: Record<string, any>
+  ): number => {
+    return (
+      Number(typeof acc === "number" ? acc : getValueByKey(acc, key)) +
+      Number(getValueByKey(curr, key))
+    );
+  };
+};
 
-export default add;
+export { sum };
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,49 @@
-import add from "../src/index.ts";
+import { sum } from "../src/index.ts";
 
-describe("add", () => {
+describe("sum", () => {
   it("is a function", () => {
-    expect(typeof add).toBe("function");
+    expect(typeof sum).toBe("function");
   });
 
-  it("adds 2 number", () => {
-    const expected = 4;
-    const actual = add(2, 2);
+  it("sums an array of numbers", () => {
+    const expected = 6;
+    const actual = [1, 2, 3].reduce(sum);
 
     expect(actual).toEqual(expected);
+  });
+
+  it("sums an array of numbers in string format", () => {
+    const expected = 6;
+    const actual = ["1", "2", 3].reduce(sum);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns NaN if array has other types", () => {
+    const expected = NaN;
+    const actual = [1, "hello", 3].reduce(sum);
+
+    expect(actual).toEqual(expected);
+  });
+
+  describe("sum.on", () => {
+    it("is a function", () => {
+      expect(typeof sum.on).toBe("function");
+    });
+
+    it("returns a function", () => {
+      expect(typeof sum.on()).toBe("function");
+    });
+
+    it("sums an array of objects", () => {
+      const expected = 111;
+      const actual = [
+        { name: "Alice", age: 25 },
+        { name: "Bob", age: 32 },
+        { name: "Candice", age: 54 },
+      ].reduce(sum.on("age"));
+
+      expect(actual).toEqual(expected);
+    });
   });
 });
