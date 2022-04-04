fd39190ee1ebc13490138dff663211b6be01086c
Antonio Villagra De La Cruz <5470315+AntonioVdlC@users.noreply.github.com>
2022-04-04 18:51:25 +0200

    Implement average

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import getValueByKey from "get-value-key";
+
+/**
+ * Calculates the sum of values in an array
+ * @param acc
+ * @param curr
+ * @returns Sum
+ */
 function sum(acc: number, curr: number): number {
   return Number(acc) + Number(curr);
 }
+/**
+ * Calculate the sum of values from a specific field in an array of objects
+ * @param key
+ * @returns
+ */
 sum.on = (key: string) => {
   return (
     acc: Record<string, any> | number,
@@ -14,4 +26,44 @@ sum.on = (key: string) => {
   };
 };
 
-export { sum };
+/**
+ * Calculates the average of values in an array
+ * @param acc
+ * @param curr
+ * @returns Average
+ */
+function average(
+  acc: number,
+  curr: number,
+  i: number,
+  arr: Array<number>
+): number {
+  return (
+    (i === 1 ? Number(acc) / arr.length : Number(acc)) +
+    Number(curr) / arr.length
+  );
+}
+/**
+ * Calculate the average of values from a specific field in an array of objects
+ * @param key
+ * @returns
+ */
+average.on = (key: string) => {
+  return (
+    acc: Record<string, any> | number,
+    curr: Record<string, any>,
+    _: never,
+    arr: Array<Record<string, any>>
+  ): number => {
+    return (
+      Number(
+        typeof acc === "number"
+          ? acc
+          : Number(getValueByKey(acc, key)) / arr.length
+      ) +
+      Number(getValueByKey(curr, key)) / arr.length
+    );
+  };
+};
+
+export { sum, average };
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import { sum } from "../src/index.ts";
+import { sum, average } from "../src/index.ts";
 
 describe("sum", () => {
   it("is a function", () => {
@@ -42,6 +42,54 @@ describe("sum", () => {
         { name: "Bob", age: 32 },
         { name: "Candice", age: 54 },
       ].reduce(sum.on("age"));
+
+      expect(actual).toEqual(expected);
+    });
+  });
+});
+
+describe("average", () => {
+  it("is a function", () => {
+    expect(typeof average).toBe("function");
+  });
+
+  it("averages an array of numbers", () => {
+    const expected = 2;
+    const actual = [1, 2, 3].reduce(average);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("averages an array of numbers in string format", () => {
+    const expected = 2;
+    const actual = ["1", "2", 3].reduce(average);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns NaN if array has other types", () => {
+    const expected = NaN;
+    const actual = [1, "hello", 3].reduce(average);
+
+    expect(actual).toEqual(expected);
+  });
+
+  describe("average.on", () => {
+    it("is a function", () => {
+      expect(typeof average.on).toBe("function");
+    });
+
+    it("returns a function", () => {
+      expect(typeof average.on()).toBe("function");
+    });
+
+    it("averages an array of objects", () => {
+      const expected = 37;
+      const actual = [
+        { name: "Alice", age: 25 },
+        { name: "Bob", age: 32 },
+        { name: "Candice", age: 54 },
+      ].reduce(average.on("age"));
 
       expect(actual).toEqual(expected);
     });
